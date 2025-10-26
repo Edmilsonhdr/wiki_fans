7d5e186077eed612b81ac1d59038c40ac4a375e8
Edmilsonhdr <seuemail@exemplo.com>
2025-10-26 20:22:45 -0300

    Add Powerstats type to GraphQL schema with fields for intelligence, strength, speed, durability, power, and combat

---
+++ backend/graphql/types/powerstats.type.js
@@ -0,0 +1,13 @@
+import { GraphQLObjectType } from 'graphql';
+// Definindo o tipo User que representa os dados vindos da API
+export const PowerstatsType = new GraphQLObjectType({
+  name: 'Powerstats',
+  fields: {
+    intelligence: { type: GraphQLInt },
+    strength: { type: GraphQLInt },
+    speed: { type: GraphQLInt },
+    durability: { type: GraphQLInt },
+    power: { type: GraphQLInt },
+    combat: { type: GraphQLInt },
+  },
+});
